f19281b12a72b93435c55bcbe668284db651f071
XyronGod <84180602+XyronGod@users.noreply.github.com>
2021-09-18 11:38:50 +0500

    Add files via upload

---
+++ WpfApp9/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp9
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                MainFrame.GoBack();
+            }
+            else
+            {
+                MessageBox.Show("нельзя вернуться назад");
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+          MainFrame.Navigate(Pages.Class1.group);
+        }
+
+        private void MainFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(Pages.Class1.teachers);
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(Pages.Class1.raspisanie);
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(Pages.Class1.lessons);
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(Pages.Class1.students);
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(Pages.Class1.journal);
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(Pages.Class1.predmeti);
+        }
+    }
+
+    
+}
